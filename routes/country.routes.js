d62e06eb043e606118a6e22df499767bb7718214
Bruno Cunha <brunorcunha3000@gmail.com>
2023-09-07 21:05:23 +0100

    cleaning code

--- routes/country.routes.js
+++ routes/country.routes.js
@@ -9,103 +9,85 @@ const { isAuthenticated } = require("../middleware/jwt.middleware");
 
 // Get Route that gets all the project
 router.get("/countries", async (req, res) => {
-    try {
-        let allCountries = await Country.find();
-        res.json(allCountries);
-    } catch (error) {
-        res.json(error);
-    }
+  try {
+    let allCountries = await Country.find();
+    res.json(allCountries);
+  } catch (error) {
+    res.json(error);
+  }
 });
 
 // Get Route that gets info of specific project
 router.get("/:countryCode", async (req, res) => {
-    const { countryCode } = req.params;
-    /* const currentUser = req.session.currentUser;*/
+  const { countryCode } = req.params;
 
-    try {
-        let foundCountry = await Country.findOne({ cca2: countryCode });
+  try {
+    let foundCountry = await Country.findOne({ cca2: countryCode });
 
-        foundCountry.populate("articles");
+    foundCountry.populate("articles");
 
-        /* await thisUser.save();
-        foundCountry = { ...foundCountry.toObject(), isFav }; */
-        res.json(foundCountry);
-    } catch (error) {
-        res.json(error);
-    }
+    res.json(foundCountry);
+  } catch (error) {
+    res.json(error);
+  }
 });
 
 // Favourite Country action
 router.post("/addFavorites/:countryCode", isAuthenticated, async (req, res) => {
-    const { countryCode } = req.params;
-    // User
-    const thisUser = req.payload;
-    const currentUser = await User.findById(thisUser._id);
-    // Get country
-    let foundCountry = await Country.findOne({ cca2: countryCode });
-    const isFav = currentUser.favoritesCountries.includes(foundCountry._id);
-    let numLikes = foundCountry.favorites;
+  const { countryCode } = req.params;
+  // User
+  const thisUser = req.payload;
+  const currentUser = await User.findById(thisUser._id);
+  // Get country
+  let foundCountry = await Country.findOne({ cca2: countryCode });
+  const isFav = currentUser.favoritesCountries.includes(foundCountry._id);
+  let numLikes = foundCountry.favorites;
 
-    try {
-        if (!isFav) {
-            currentUser.favoritesCountries.push(foundCountry._id);
-            await Country.findByIdAndUpdate(foundCountry._id, {
-                favorites: numLikes + 1,
-            });
-            //console.log("add");
-        } else {
-            currentUser.favoritesCountries.pull(foundCountry._id);
-            await Country.findByIdAndUpdate(foundCountry._id, {
-                favorites: numLikes - 1,
-            });
-            //console.log("remove");
-        }
-        /* const favCountry = await User.findByIdAndUpdate(
-            "64f0cd0d4dd998b6120c21b9",
-            {
-                $push: { favoritesCountries: idCountry },
-            }
-        ); */
-        await currentUser.save();
-
-        res.json(!isFav);
-    } catch (error) {
-        res.json(error);
+  try {
+    if (!isFav) {
+      currentUser.favoritesCountries.push(foundCountry._id);
+      await Country.findByIdAndUpdate(foundCountry._id, {
+        favorites: numLikes + 1,
+      });
+    } else {
+      currentUser.favoritesCountries.pull(foundCountry._id);
+      await Country.findByIdAndUpdate(foundCountry._id, {
+        favorites: numLikes - 1,
+      });
     }
+    await currentUser.save();
+
+    res.json(!isFav);
+  } catch (error) {
+    res.json(error);
+  }
 });
 
 // Visited Country action
 router.post("/addVisited/:countryCode", isAuthenticated, async (req, res) => {
-    const { countryCode } = req.params;
-    // User
-    const thisUser = req.payload;
-    const currentUser = await User.findById(thisUser._id);
-    // Get country
-    let foundCountry = await Country.findOne({ cca2: countryCode });
-    const isVisited = currentUser.visitedCountries.includes(foundCountry._id);
+  const { countryCode } = req.params;
+  // User
+  const thisUser = req.payload;
+  const currentUser = await User.findById(thisUser._id);
+  // Get country
+  let foundCountry = await Country.findOne({ cca2: countryCode });
+  const isVisited = currentUser.visitedCountries.includes(foundCountry._id);
 
-    try {
-        if (!isVisited) {
-            currentUser.visitedCountries.push(foundCountry._id);
-            await Country.findByIdAndUpdate(foundCountry._id, {});
-            //console.log("add");
-        } else {
-            currentUser.visitedCountries.pull(foundCountry._id);
-            await Country.findByIdAndUpdate(foundCountry._id, {});
-            //console.log("remove");
-        }
-        /* const favCountry = await User.findByIdAndUpdate(
-            "64f0cd0d4dd998b6120c21b9",
-            {
-                $push: { favoritesCountries: idCountry },
-            }
-        ); */
-        await currentUser.save();
-
-        res.json(!isVisited);
-    } catch (error) {
-        res.json(error);
+  try {
+    if (!isVisited) {
+      currentUser.visitedCountries.push(foundCountry._id);
+      await Country.findByIdAndUpdate(foundCountry._id, {});
+    } else {
+      currentUser.visitedCountries.pull(foundCountry._id);
+      await Country.findByIdAndUpdate(foundCountry._id, {});
     }
+
+    await currentUser.save();
+
+    res.json(!isVisited);
+  } catch (error) {
+    res.json(error);
+  }
 });
 
 module.exports = router;
